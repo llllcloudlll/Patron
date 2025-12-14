e120dc339820c4290640c8910f9d46194dab55ef
llllcloudlll <cloud.ibo7@gmail.com>
2025-12-15 01:12:55 +0300

    Refactor service worker install and activate events

--- sw.js
+++ sw.js
@@ -1,9 +1,2 @@
-// Patron Gym OS - Service Worker
-// Çevrimdışı çalışmayı ve önbelleklemeyi yönetir.
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Kuruldu');
-});
-
-self.addEventListener('fetch', (e) => {
-    // Basit fetch işlemi
-});
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => self.clients.claim());
